787b80bb02909112eacaab3e507715b10d3d7c4c
Samara Jacobo <sjacobo3@nd.edu>
2025-04-09 02:14:56 -0400

    features 3.1 and 3.2

--- src/course_project/campusmart/models.py
+++ src/course_project/campusmart/models.py
@@ -1,15 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=200)
-    username = models.CharField(max_length=200, unique=True)
-    email = models.EmailField(max_length=200, unique=True)
-    password = models.TextField()
-
-    def __str__(self):
-        return f"Username: {self.username}; Email: {self.email}"
-
 class Listing(models.Model):
     CONDITION_CHOICES = [
         ('New', 'New'),
@@ -23,6 +15,7 @@ class Listing(models.Model):
         ('Unavailable', 'Unavailable'),
     ]
 
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
--- src/course_project/campusmart/urls.py
+++ src/course_project/campusmart/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'campusmart'
@@ -6,20 +9,32 @@ app_name = 'campusmart'
 urlpatterns = [
     # /campusmart/index
     path('index/', views.index, name='index'),
+
     # /campusmart/register
     path('register/', views.register, name='register'),
+
     # /campusmart/login
     path('login/', views.login, name='login'),
+
     # /campusmart/logout
     path('logout/', views.logout, name='logout'),
+
     # /campusmart/dashboard
-    path('dashboard/', views.dashboard, name='dashboard'), 
+    # path('dashboard/', views.dashboard, name='dashboard'), 
     # /campusmart/create
     path('create_listing/', views.create_listing, name='create_listing'),
+
     # /campusmart/update
-    path('update_listing/', views.update_listing, name='update_listing'),
+    path('update_listing/<int:listing_id>', views.update_listing, name='update_listing'),
+
     # /campusmart/delete
-    path('delete_listing/', views.delete_listing, name='delete_listing'),
-]
+    path('delete_listing/<int:listing_id>', views.delete_listing, name='delete_listing'),
+
+    # /campusmart/listings
+    path('listings/', views.listing_all, name='listing_all'),
+    
+    # /campusmart/listings/pk
+    path('listings/<int:pk>', views.detail, name='detail')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
     
